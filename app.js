d297aef2df163788708166efb72f302328b4c582
Bankole Ahmed <kidrolex19@gmail.com>
2020-06-03 02:22:35 +0100

    Deploy to github

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express');
+const axios = require('axios');
+let token = null;
+const app = express();
+
+const clientId = process.env.GITHUB_CLIENT_ID
+const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+
+app.get('/', (req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}`);
+});
+
+
+
+app.get('/oauth-callback', (req, res) => {
+  const body = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    code: req.query.code
+  };
+  const opts = { headers: { accept: 'application/json' } };
+  axios.post(`https://github.com/login/oauth/access_token`, body, opts).
+    then(res => res.data['access_token']).
+    then(_token => {
+      console.log('My token:', token);
+      token = _token;
+      res.json({ ok: 1 });
+    }).
+    catch(err => res.status(500).json({ message: err.message }));
+});
+
+app.listen(3300);
+console.log('App listening on port 3300');
